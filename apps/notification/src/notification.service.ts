e7de721a40a7570dfbaebef4872d6257c1a581da
Dominik Ilski <dominikk.ilski@gmail.com>
2023-10-29 20:54:42 +0100

    Added: message process ack

--- apps/notification/src/notification.service.ts
+++ apps/notification/src/notification.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { NotificationMessageDto } from './dto/notification.dto';
 import { RmqContext } from '@nestjs/microservices';
+import { RmqService } from '@app/common/rabbitMQ';
 
 @Injectable()
 export class NotificationService {
+  constructor(private rmqService: RmqService) {}
   private readonly logger = new Logger(NotificationService.name);
   sendNotification(data: NotificationMessageDto, context: RmqContext) {
     this.logger.log(data.message, data.email);
     console.log('context', context.getChannelRef);
+    this.rmqService.ack(context);
   }
 }
